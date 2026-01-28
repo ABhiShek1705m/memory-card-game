215c339826ffb1c00011b2f6a157bcf8c7a2e428
Abhishek Majumdar <abhishekmajumdar@uvic.ca>
2026-01-28 15:13:28 -0800

    removed vercel.json and he deprecated ImageContext references

--- client/src/app/play/layout.jsx
+++ client/src/app/play/layout.jsx
@@ -1,18 +1,15 @@
 'use client'
 
-import ImageContext from "../../context/ImageContext";
 import { useMemo } from "react";
 
 export default function GameLayout({ children }){
-    //memoized to skip re-rendering
-    const imgArray = useMemo(() => new Array(16).fill(null), [])
+    //memoized to skip re-rendering (no images for now)
+    // const imgArray = useMemo(() => new Array(16).fill(null), [])
 
     return(
         <>
         <div className="h-[95vh] w-[95vw] border-5 [border-style: outset] border-yellow-700 relative top-4 left-9">
-        <ImageContext.Provider value={imgArray}>
             {children}
-        </ImageContext.Provider>
         </div>
         </>
     )
